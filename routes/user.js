a9d6cbc99a09c7a5fb2d7c2a0e3128e9a1e2cac8
Ian Campbell <iancam@stanford.edu>
2014-02-26 12:16:25 -0800

    share removes people

--- routes/user.js
+++ routes/user.js
@@ -3,28 +3,37 @@ var mongoose = require("mongoose");
 var app = require("../app");
 var models = require("../models");
 var helpers = require('../helpers');
-var underscore = require('underscore');
+var _ = require('underscore');
 exports.new_user = function (req, res) {
   res.redirect("/my_pantries");
 };
 
-
-
-
 exports.myPantries = function (req, res) {
-  
+	models.Pantry.find({'invited_emails': req.user.email}, function(err, found_pantries) {
+    _.each(found_pantries, function(pantry, index, list) {
+      var add_user = true;
+      _.all(pantry.users, function(user, index, list) {
+        if (user.equals(req.user._id)) {
+          add_user = false;
+          return false;
+        }
+      });
+      if (add_user) {
+        pantry.users.push(req.user._id);
+      };
+      var ei = pantry.invited_emails.indexOf(req.user.email);
+      pantry.invited_emails.splice(ei, 1);
+      pantry.save(helpers.error);
+    });
+  });
   models.Pantry.find({'users': req.user._id})
   .sort('name')
+  .populate('users')
   .exec(function (err, found_pantries) {
     if (err) helpers.error(err);
-    models.Pantry.find({'invited_emails': req.user.email}, function (err, shared_pantries){
-     
-      res.render('pantries', 
-      {user: req.user,
-       pantries: _.union(shared_pantries, found_pantries),
-       invited_to: shared_pantries
-      });
-    });
+    res.render('pantries', 
+    {user: req.user,
+     pantries: found_pantries});
   });
 }
 
