2502c526dd73821313fa18b6a58e4dcebf0aa102
andylamb <andrew.lamb@stanford.edu>
2014-02-28 01:04:46 -0800

    Committing for heroku

--- app.js
+++ app.js
@@ -69,8 +69,8 @@ var server = email.server.connect({
 passport.use(new FacebookStrategy({
 		clientID: "220032974854303",
 		clientSecret: "3f3ca3266c18ee0911a845526023b593",
-		callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
-		// callbackURL: "http://safe-anchorage-2842.herokuapp.com/auth/facebook/callback"
+		// callbackURL: "http://127.0.0.1:3000/auth/facebook/callback"
+		callbackURL: "http://safe-anchorage-2842.herokuapp.com/auth/facebook/callback"
 	},
 	function(accessToken, refreshToken, profile, done) {
 		var newUser = {fid: profile.id,
