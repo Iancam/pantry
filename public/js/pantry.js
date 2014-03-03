1f342a8cc1d1ea2ae1cd182304ab27987cbe005c
andylamb <andrew.lamb@stanford.edu>
2014-03-02 18:41:16 -0800

    Edit on pantry_test now works

--- public/js/pantry.js
+++ public/js/pantry.js
@@ -1,15 +1,16 @@
-$('.btn-remove').click(function () {
-  var item = $(this).parent('.item')
-  var id = item.attr('id');
-  $.post('/remove_item', {id: id}, function (data) {
+$(".btn-remove").click(function () {
+  console.log($(this));
+  var item = $(this).parent(".item")
+  var id = item.attr("id");
+  $.post("/remove_item", {id: id}, function (data) {
     item.remove();
   })
 })
 
-$('#edit').click(function () {
-  $('.btn-remove').toggle(50);
+$("#edit").click(function () {
+  $(".btn-remove").toggle(50);
 })
 
 $(document).ready(function () {
-  $('.btn-remove').hide();
+  $(".btn-remove").hide();
 })
