b4f01d6099f6bdbb23881fc2adf3201a6f289355
andylamb <andrew.lamb@stanford.edu>
2014-03-04 22:07:03 -0800

    Fixed route problems

--- app.js
+++ app.js
@@ -138,20 +138,20 @@ app.get("/pantry_alt/:id/", function (req, res) {
 })
 
 
-app.get("/shopping_list/:id/:order/", shopping_list.view);
+app.get("/shopping_list/:id/:order", shopping_list.view);
 app.get("/shopping_list/:id/", function (req, res) {
 	// Make name the order if there isn"t one.
 	var id = req.param("id");
-	res.redirect("/shopping_list/" + id + "/Name/");
+	res.redirect("/shopping_list/" + id + "/Name");
 });
 
 
 //Shopping List Alternate
-app.get("/shopping_list/alt/:id/:order/", shopping_list.view_alt);
+app.get("/shopping_list/alt/:id/:order", shopping_list.view_alt);
 app.get("/shopping_list/alt/:id/", function (req, res) {
 	// Make name the order if there isn"t one.
 	var id = req.param("id");
-	res.redirect("/shopping_list/alt/" + id + "/Name/");
+	res.redirect("/shopping_list/alt/" + id + "/Name");
 });
 
 
